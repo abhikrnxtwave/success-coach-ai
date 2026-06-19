09c5cb1586462f3e4ee95b1a0b3daf9460f72d44
Abhishek <nxtwave@Nxtwaves-MacBook-Pro.local>
2026-06-19 12:10:28 +0530

    LLM setup to LangChain

--- config/llm.py
+++ config/llm.py
@@ -1,24 +1,19 @@
 import os
 
 from dotenv import load_dotenv
-from openai import OpenAI
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
-client = OpenAI(
-    api_key=os.getenv("OPENAI_API_KEY")
-)
-
 MODEL_NAME = os.getenv(
     "MODEL_NAME",
-    "gpt-5"  #fallback if Model_Name is not in .env file.
+    "gpt-5"  # fallback if MODEL_NAME is not in .env
 )
 
+llm = ChatOpenAI(
+    model=MODEL_NAME
+)
 
 def get_ai_response(messages):
-    response = client.chat.completions.create(
-        model=MODEL_NAME,
-        messages=messages
-    )
-
-    return response.choices[0].message.content
+    response = llm.invoke(messages)
+    return response.content
